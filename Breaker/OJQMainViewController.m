5e368d50ccdd7f1bc2e7f94cb5ca4d6dff7b896c
oujiaqi <471884121@qq.com>
2017-12-06 08:34:50 +0800

    home

---
+++ Breaker/OJQMainViewController.m
@@ -0,0 +1,42 @@
+//
+//  OJQMainViewController.m
+//  Breaker
+//
+//  Created by ouou on 2017/12/6.
+//  Copyright © 2017年 oujiaqi. All rights reserved.
+//
+
+#import "OJQMainViewController.h"
+
+@interface OJQMainViewController ()
+
+@end
+
+@implementation OJQMainViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Breaker/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  Breaker
+//
+//  Created by ouou on 2017/12/6.
+//  Copyright © 2017年 oujiaqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
